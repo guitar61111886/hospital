19bb929fdb326bfccb62338a86ae0f9de2af6888
guitar61111886 <guitarthunrada@gmail.com>
2021-12-22 16:32:54 +0700

    project

---
+++ customer.js
@@ -0,0 +1,11 @@
+class Customer{
+    constructor(CustomerID,Firstname,Lastname,Age,Gender){
+        this.CustomerID = CustomerID;
+        this.Firstname = Firstname;
+        this.Lastname = Lastname;
+        this.Age = Age;
+        this.Gender = Gender;
+    }
+}
+
+module.exports = Customer;
---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+    user : 'gg123',
+    password: '123',
+    server: '127.0.0.1',
+    database: 'SQL',
+    // option:{
+    //     trustedConnection: true,
+    //     enableArithPort: true,
+    //     instancename: 'SQLEXPRESS'
+    // },
+    port : 1433
+}
+
+module.exports = config;
